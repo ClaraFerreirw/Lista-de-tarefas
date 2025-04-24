d059e5c845581f907f75add350bbce5b2e2f6e3a
ClaraFerreirw <ferreira.silva.maria2003@escola.pr.gov.br>
2025-04-24 11:54:47 -0300

    ok

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+document.addEventListener('DOMContentLoaded', () => {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
 
-checkboxes.forEach((checkbox, index) => {
-  const saved = localStorage.getItem(`tarefa-${index}`);
-  if (saved === "true") checkbox.checked = true;
+  checkboxes.forEach((checkbox, index) => {
+    const saved = localStorage.getItem(`tarefa-${index}`);
+    if (saved === "true") checkbox.checked = true;
 
-  checkbox.addEventListener('change', () => {
-    localStorage.setItem(`tarefa-${index}`, checkbox.checked);
+    checkbox.addEventListener('change', () => {
+      localStorage.setItem(`tarefa-${index}`, checkbox.checked);
+    });
   });
 });
